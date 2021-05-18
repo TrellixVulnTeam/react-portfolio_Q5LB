e6ffd5e6d91fc6f8d3b2d2c56ad4fd793f4454ad
E-Martinez77 <emartizzle2185@me.com>
2021-05-18 15:48:07 -0600

    deployment attempt #40

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import HomePage from "./components/home-page/index";
 import PortfolioPage from "./components/portfolio-page/index";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/portfolio/index.html" component={PortfolioPage} />
